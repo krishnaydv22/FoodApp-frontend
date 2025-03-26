f8c7149c898c85cc363438ba21f2db9f2f429bac
Krushna <krishnaydv35@gmail.com>
2025-03-26 18:21:15 +0530

    addedd backend url

--- src/Components/config/api.js
+++ src/Components/config/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_URL = "http://localhost:5454"
+export const API_URL = "https://striking-energy-production.up.railway.app/"
 
 export const api = axios.create({
     baseURL:API_URL,
