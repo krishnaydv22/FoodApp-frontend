e2b05b9d155a2ef909e6860cd3adcb3f0e98de3e
Krushna <krishnaydv35@gmail.com>
2025-03-26 20:34:59 +0530

    changed url

--- src/Components/config/api.js
+++ src/Components/config/api.js
@@ -1,6 +1,6 @@
     import axios from "axios"
 
-    export const API_URL = "https://striking-energy-production.up.railway.app"
+    export const API_URL = "https://bubbly-curiosity-production.up.railway.app"
 
     export const api = axios.create({
         baseURL:API_URL,
