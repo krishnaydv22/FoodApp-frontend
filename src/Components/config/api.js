e4ed35a1efcefbabc6d68232f63a5c24d3dde552
Krushna <krishnaydv35@gmail.com>
2025-03-26 19:19:07 +0530

    added withCredentials:

--- src/Components/config/api.js
+++ src/Components/config/api.js
@@ -1,10 +1,13 @@
-import axios from "axios"
+    import axios from "axios"
 
-export const API_URL = "https://striking-energy-production.up.railway.app/"
+    export const API_URL = "https://striking-energy-production.up.railway.app"
 
-export const api = axios.create({
-    baseURL:API_URL,
-    headers:{
-        "Content-Type":"application/json"
-    }
-})
+    export const api = axios.create({
+        baseURL:API_URL,
+        headers:{
+            "Content-Type":"application/json"
+        },
+        withCredentials: true
+    })
+
+   
